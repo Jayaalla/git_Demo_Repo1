62bd17ae11239c018eac4134df1d70253b046dfa
Jayaalla <Jayaalla@gmail.com>
2020-04-14 23:32:33 -0500

    second commit done as per request

--- src/git_Demo/Demo.java
+++ src/git_Demo/Demo.java
@@ -25,10 +25,20 @@ public class Demo {
 		
 }
 }
+/* TODO Auto-generated method stub
+int n,a=1,b=1;
+n=0;
 
-	
+System.out.println("1\n1");
+while(n<100)
+{
+	n=a+b;
+	System.out.println(n + " ");
+	a=b;
+	b=n;
+	*/
 	
 	
 	
 
-}
+
