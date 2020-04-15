4aac5549d8bb6c6610e5056681ef9f9f6e2d46f6
Jayaalla <Jayaalla@gmail.com>
2020-04-14 23:00:44 -0500

    first commit of my demo

---
+++ src/git_Demo/Demo.java
@@ -0,0 +1,34 @@
+package git_Demo;
+
+import java.util.Scanner;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int n=0;
+		System.out.println("Enter the number");
+		n=sc.nextInt();
+		if(n>0)
+		{
+			System.out.println(n+" is positive.");
+		}	
+		else if(n<0)
+		{
+			System.out.println(n+" is negative");
+		}
+		else 
+		{
+		    System.out.println("The number is zero ");	
+		}
+		
+}
+}
+
+	
+	
+	
+	
+
+}
